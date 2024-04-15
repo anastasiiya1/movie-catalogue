9ad626152ec569ccdf9c4cb5f1a8ee07e3e67fdd
anastasiiya1 <149606692+anastasiiya1@users.noreply.github.com>
2024-04-15 19:01:18 +0000

    Deploying to gh-pages from @ anastasiiya1/pagination@456a7f0c817164e8fe31f9dcdb8a5e8157c76a4f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,16 +1,16 @@
-import{a as m}from"./assets/vendor-af591e2d.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const h="https://api.themoviedb.org/3",y="/trending/movie/week",b="598d894135bb5dfa4bf6d8e6fe6006eb",l={container:document.querySelector(".js-movie-list"),loadBtn:document.querySelector(".js-load-more"),guard:document.querySelector(".js-guard")};let u=1,v={root:null,rootMargin:"100px",threshold:1},L=new IntersectionObserver($,v);f(u).then(r=>{l.container.insertAdjacentHTML("beforeend",d(r.results)),console.log(r),r.page<500&&L.observe(l.guard)}).catch(r=>console.log(r.message));async function f(r=1){const{data:s}=await m(`${h}${y}`,{params:{api_key:b,page:r}});return s}function d(r){return r.map(({id:s,poster_path:n,original_title:o,release_date:e,vote_average:t})=>{const a=Math.floor(t/2),p=t%2!==0,g=5-Math.ceil(t/2);let c="";for(let i=0;i<a;i++)c+='<use href="./img/rating.svg#stars-full-star"></use>';p&&(c+='<use href="./img/rating.svg#stars-half-star"></use>');for(let i=0;i<g;i++)c+='<use href="./img/rating.svg#stars-empty-star"></use>';return`
-            <li class='movie-card' data-id='${s}'>
-              <img src='https://image.tmdb.org/t/p/w500${n}' alt='${o}'>
-              <div class='movie-info'>
-                  <h2>${o}</h2>
-                  <p>Release date: ${e}</p>
-                  <p class="rating-container">
-                      <svg aria-hidden="true" focusable="false" class="rating">
-                          ${c}
-                      </svg>
-                  </p>
-                  <span class='vote-average'>${Math.floor(t*10)/10}</span>
-              </div>
-            </li>
-          `}).join("")}function $(r,s){r.forEach(async n=>{if(n.isIntersecting){u+=1;try{const o=await f(u);l.container.insertAdjacentHTML("beforeend",d(o.results)),o.page>=500&&s.unobserve(n.target)}catch{alert(error.message)}}})}l.container.addEventListener("click",openCard);
+import{a as h}from"./assets/vendor-af591e2d.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const g="https://api.themoviedb.org/3",y="/trending/movie/week",b="598d894135bb5dfa4bf6d8e6fe6006eb",l={container:document.querySelector(".js-movie-list"),loadBtn:document.querySelector(".js-load-more"),guard:document.querySelector(".js-guard")};let u=1,v={root:null,rootMargin:"100px",threshold:1},$=new IntersectionObserver(L,v);f(u).then(r=>{l.container.insertAdjacentHTML("beforeend",d(r.results)),console.log(r),r.page<500&&$.observe(l.guard)}).catch(r=>console.log(r.message));async function f(r=1){const{data:s}=await h(`${g}${y}`,{params:{api_key:b,page:r}});return s}function d(r){return r.map(({id:s,poster_path:n,original_title:o,release_date:e,vote_average:t})=>{const a=Math.floor(t/2),p=t%2!==0,m=5-Math.ceil(t/2);let c="";for(let i=0;i<a;i++)c+='<use href="#stars-full-star"></use>';p&&(c+='<use href="#stars-half-star"></use>');for(let i=0;i<m;i++)c+='<use href="#stars-empty-star"></use>';return`
+              <li class='movie-card' data-id='${s}'>
+                <img src='https://image.tmdb.org/t/p/w500${n}' alt='${o}'>
+                <div class='movie-info'>
+                    <h2>${o}</h2>
+                    <p>Release date: ${e}</p>
+                    <p class="rating-container">
+                        <svg aria-hidden="true" focusable="false" class="rating">
+                            ${c}
+                        </svg>
+                    </p>
+                    <span class='vote-average'>${Math.floor(t*10)/10}</span>
+                </div>
+              </li>
+            `}).join("")}function L(r,s){r.forEach(async n=>{if(n.isIntersecting){u+=1;try{const o=await f(u);l.container.insertAdjacentHTML("beforeend",d(o.results)),o.page>=500&&s.unobserve(n.target)}catch{alert(error.message)}}})}
 //# sourceMappingURL=commonHelpers.js.map
